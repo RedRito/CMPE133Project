f7a03a9fb351c41f9152b5479af3f988d0690cad
RedRito <brandonluong2494@gmail.com>
2022-03-29 18:38:26 -0700

    Created page changing example through onClick. Need more pages to continue.

--- app/src/main/java/com/example/simpleapptodolist/MainActivity.kt
+++ app/src/main/java/com/example/simpleapptodolist/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.simpleapptodolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_main)
+        val text: TextView = findViewById(R.id.tv_signup)
+        text.setOnClickListener{
+            val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
