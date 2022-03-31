d558cde583b42669b0818ee862fdce7595b7be47
razzacktiger <harrazzack@gmail.com>
2022-03-31 16:28:46 -0700

    Added search activity file with xml layout based on prebuilt scroll and search UI

---
+++ app/src/main/java/com/example/simpleapptodolist/SearchActivity.kt
@@ -0,0 +1,27 @@
+package com.example.simpleapptodolist
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.simpleapptodolist.databinding.SearchMainBinding
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: SearchMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = SearchMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
