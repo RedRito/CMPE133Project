f2375b10b3441c55d447329eb5dcee55540f523a
Razzacktiger <harrazzack@gmail.com>
2022-04-07 18:06:26 -0700

    Added and incorporated SearchVeiw Fragment to homepage

--- app/src/main/java/com/example/simpleapptodolist/HomePageActivity.kt
+++ app/src/main/java/com/example/simpleapptodolist/HomePageActivity.kt
@@ -16,7 +16,7 @@ class HomePageActivity : AppCompatActivity() {
         //create a variable for each Fragment
         val articlesFragment = ArticlesFragment()
         val homeFragment = HomeFragment()
-
+        val searchFragment = SearchFragment()
 
 
         //first Fragment that is shown
@@ -26,6 +26,7 @@ class HomePageActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.home  -> setCurrentFragment(homeFragment)
                 R.id.profile -> setCurrentFragment(articlesFragment)
+                R.id.search -> setCurrentFragment(searchFragment)
             }
             true
         }
