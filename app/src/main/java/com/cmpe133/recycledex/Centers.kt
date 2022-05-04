29e2f55e0690ee025abf3785281918d2812ad365
pauline <paulinegaringan@gmail.com>
2022-05-04 00:49:03 -0700

    added location search xml files and Centers data class

---
+++ app/src/main/java/com/cmpe133/recycledex/Centers.kt
@@ -0,0 +1,12 @@
+package com.cmpe133.recycledex
+
+class Centers (var rcname: String = "",
+               var address: String = "",
+               var hours: String = "",
+               var lat: String = "",
+               var lon: String = "",
+               var accepted: String = "",
+               var phoneNum: String = "",
+               var link: String = "")
+{
+}
