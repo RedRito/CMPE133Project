29047757742d06d52384f56741e49d92ed1e23c4
pauline <paulinegaringan@gmail.com>
2022-05-12 14:54:40 -0700

    added favorites cardview

--- app/src/main/java/com/cmpe133/recycledex/Centers.kt
+++ app/src/main/java/com/cmpe133/recycledex/Centers.kt
@@ -1,12 +1,12 @@
 package com.cmpe133.recycledex
 
-class Centers (var rcname: String = "",
-               var address: String = "",
-               var hours: String = "",
-               var lat: String = "",
-               var lon: String = "",
+class Centers (var name: String = "",
+               var location: String = "",
+               var description: String = "",
+               var lat: Double = 0.0,
+               var lon: Double = 0.0,
                var accepted: String = "",
-               var phoneNum: String = "",
+               var phone: String = "",
                var link: String = "")
 {
 }
