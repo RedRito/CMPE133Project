cd2bbbb6f35c9cf9e14a748657d838206785b295
Brandon <brandonluong2494@gmail.com>
2022-05-15 03:40:35 -0700

    Changed the toast's in a few files, added recyclerview to user profile

--- app/src/main/java/com/cmpe133/recycledex/ProfileFragment.kt
+++ app/src/main/java/com/cmpe133/recycledex/ProfileFragment.kt
@@ -11,12 +11,14 @@ import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.text.trimmedLength
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cmpe133.recycledex.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_profile.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,8 +34,9 @@ class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-
-    private lateinit var user: User
+    private lateinit var favArrayList: ArrayList<Centers>
+    private lateinit var centersRecyclerView: RecyclerView
+    private lateinit var bundle: Bundle
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +51,7 @@ class ProfileFragment : Fragment() {
         }
         else
         {
-            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Log In To See Data", Toast.LENGTH_SHORT).show()
         }
 
 
@@ -65,8 +68,16 @@ class ProfileFragment : Fragment() {
         }
         else
         {
+            binding.btnLogout.text = "LOGIN"
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun roundDown( num : Double) : String{
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val round =  df.format(num)
+        return round.toString()
     }
 
 
@@ -85,27 +96,83 @@ class ProfileFragment : Fragment() {
                 var trimEmail = email.toString()
                 if(email.toString().length > 10)
                 {
-                    trimEmail = email.toString().substring(0, 10) + email.toString().substring(email.toString().indexOf('@'), email.toString().length)
+                    val to = email.toString().indexOf('@')
+                    trimEmail = if(to < 10) {
+                        email.toString().substring(0, email.toString().indexOf('@')) + email.toString().substring(email.toString().indexOf('@'), email.toString().length)
+                    } else {
+                        email.toString().substring(0, 10) + email.toString().substring(email.toString().indexOf('@'), email.toString().length)
+                    }
                 }
+
                 binding.tvEmail.text = trimEmail
-                binding.tvTotalemiss.text = emiss.toString() + " kg"
-                binding.tvPlasticAmount.text = plastic.toString() + " kg"
-                binding.tvWoodAmount.text = paper.toString() + " kg"
-                binding.tvMetalAmount.text = metal.toString() + " kg"
-                binding.tvElectAmount.text = elect.toString() + " kg"
-                binding.tvClothesAmount.text = glass.toString() + " kg"
+                binding.tvTotalemiss.text = roundDown(emiss.toString().toDouble()) + " kg"
+                binding.tvPlasticAmount.text = roundDown(plastic.toString().toDouble()) + " kg"
+                binding.tvWoodAmount.text = roundDown(paper.toString().toDouble()) + " kg"
+                binding.tvMetalAmount.text = roundDown(metal.toString().toDouble()) + " kg"
+                binding.tvElectAmount.text = roundDown(elect.toString().toDouble()) + " kg"
+                binding.tvClothesAmount.text = roundDown(glass.toString().toDouble()) + " kg"
             }
             else
             {
                 Toast.makeText(context, "User does not exist", Toast.LENGTH_SHORT).show()
             }
         }.addOnFailureListener{
-            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Login To See Data", Toast.LENGTH_SHORT).show()
         }
 
 
     }
 
+    private fun getUserFav(uid: String)
+    {
+        favArrayList.clear()
+        database = FirebaseDatabase.getInstance().getReference("Users")
+
+        database.child(uid).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists())
+                {
+                    for(centers in snapshot.child("favCenters").children)
+                    {
+                        val center = centers.getValue(Centers::class.java)
+                        favArrayList.add(center!!)
+                    }
+                    val adapter = CentersFragmentAdapter(favArrayList)
+                    centersRecyclerView.adapter = adapter
+                    adapter.setOnItemClickListener(object : CentersFragmentAdapter.onItemClickListener{
+                        override fun onItemClick(position: Int) {
+                            val rcname = favArrayList[position].name
+                            val location = favArrayList[position].location
+                            val hours = favArrayList[position].description
+                            val phone = favArrayList[position].phone
+                            val materials = favArrayList[position].accepted
+                            val link = favArrayList[position].link
+                            val lat = favArrayList[position].lat
+                            val lon = favArrayList[position].lon
+
+                            bundle.putString("rcname", rcname)
+                            bundle.putString("location", location)
+                            bundle.putString("hours", hours)
+                            bundle.putString("phone", phone)
+                            bundle.putString("materials", materials)
+                            bundle.putString("link", link)
+                            bundle.putDouble("lat", lat)
+                            bundle.putDouble("lon", lon)
+                            val fragment = MapsFragment()
+                            fragment.arguments = bundle
+                            fragmentManager?.beginTransaction()?.replace(R.id.flFragment, fragment)?.commit()
+                        }
+
+                    })
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -114,11 +181,20 @@ class ProfileFragment : Fragment() {
         //val view = inflater!!.inflate(R.layout.fragment_profile, container, false)
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
-        val args = this.arguments
-        val inputData = args?.get("rcname")
-        //tvProfile.text = inputData.toString()
-        Toast.makeText(context, inputData.toString(), Toast.LENGTH_SHORT).show()
         //MUST USE onClicks in onCreateView, otherwise it will register as NULL after the onCreate!!
+        centersRecyclerView = binding.rcFavLocProfile
+        centersRecyclerView.layoutManager = LinearLayoutManager(context)
+        centersRecyclerView.setHasFixedSize(true)
+        favArrayList = arrayListOf<Centers>()
+        auth = FirebaseAuth.getInstance()
+        bundle = Bundle()
+        var uid : String? = null
+        if(auth.currentUser != null)
+        {
+            uid = auth.currentUser?.uid
+            getUserFav(uid!!)
+
+        }
         binding.btnLogout.setOnClickListener {
             auth.signOut()
             val intent = Intent(context, MainActivity::class.java)
